b610b5b1d2e740323483dfe7344b32646ccc6f68
rajesh <rajeshgovindan777@gmail.com>
2026-08-31 15:53:06 +0530

    added docker steps

--- services/cart-service/index.js
+++ services/cart-service/index.js
@@ -47,5 +47,5 @@ app.delete("/carts/:userId", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`cart-service listeningsss on :${PORT}`);
+  console.log(`cart-service listeningss on :${PORT}`);
 });
