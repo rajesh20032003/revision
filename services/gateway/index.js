8aadd9d2d786f1d58f2dea916d89bc6a021287d8
rajesh <rajeshgovindan777@gmail.com>
2026-09-03 12:44:43 +0530

    modified jenkinsfile

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -83,5 +83,5 @@ app.post("/api/orders", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`gateway listensingss on :${PORT}`);
+  console.log(`gateway listenssingss on :${PORT}`);
 });
